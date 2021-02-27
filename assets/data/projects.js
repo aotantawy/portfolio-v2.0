3212d713ff8da9048fb7f6bb668a878e5bfe1a69
aotantawy <ahmedosama9100@gmail.com>
2021-02-27 10:35:46 +0200

    modify data structure in projects and skills

---
+++ assets/data/projects.js
@@ -0,0 +1,76 @@
+const projects = {
+  projects: [
+    {
+      id: "trie",
+      title: "Trie",
+      src: "/assets/projects-pics/trie.png",
+      description:
+        "an online travel agency that enables any tourist to book any service from the website such (restaurants, hotels, and cinemas).",
+      tools: [
+        "React js ",
+        "Redux",
+        "react full page",
+        "SASS",
+        "bootstrap",
+        "axios",
+        "node js",
+        "express js",
+        "mongodb",
+        "mongoose",
+        "nodemailer",
+        "dotenv",
+      ],
+    },
+    {
+      id: "keeper",
+      title: "Keeper",
+      src: "/assets/projects-pics/keeper.png",
+      description:
+        "just like google keep, keeper is a place where you can save all your quick notes and put all your ideas out of your head.",
+      tools: ["React js ", "Redux", "SASS", "Material ui"],
+    },
+    {
+      id: "garment",
+      title: "Garment IO",
+      src: "/assets/projects-pics/garment.png",
+      description:
+        "Search for any business you want just by his name as well as location the search result will be location, reviews, rating, and full address",
+      tools: ["React js", "Redux", "SASS", "bootstrap", "axios", "dotenv"],
+    },
+    {
+      id: "corona",
+      title: "Corona",
+      src: "/assets/projects-pics/corona.png",
+      description:
+        "view coronavirus stats in a simple bar chart and also can search for stats in any certain country.",
+      tools: ["React js ", "chart js", "lodash"],
+    },
+    {
+      id: "textEditor",
+      title: "Text Editor",
+      src: "/assets/projects-pics/textEditor.png",
+      description:
+        "a simple text editor with a nice web interface that has the basic functionalities like save, update and view all files.",
+      tools: [
+        "html",
+        "css",
+        "javaScript",
+        "node js",
+        "express",
+        "ejs",
+        "mongodb",
+        "mongoose",
+      ],
+    },
+    {
+      id: "ask",
+      title: "Ask",
+      src: "/assets/projects-pics/ask.png",
+      description:
+        "Q&A website to exchange knowledge through asking questions and for each question, there are set of options like (add like, and dislike, add a comment)",
+      tools: ["React js ", "css", "Material ui"],
+    },
+  ],
+};
+
+export default projects;
