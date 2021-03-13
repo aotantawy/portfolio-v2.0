e4bf2b05d96113b5f139da5f92b13e3a1018ed48
aotantawy <ahmedosama9100@gmail.com>
2021-03-13 06:06:07 +0200

    add up button and remove fade animation

--- assets/index.js
+++ assets/index.js
@@ -5,3 +5,18 @@ import populateProjects from "./js/populate-projects-data.js";
 populateAbout();
 populateSkills();
 populateProjects();
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// scroll up animation when click
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.onclick = scrollToTop;
+});
+
+// scroll up by click up button
+document.querySelector(".scroll-up").onclick = scrollToTop;
